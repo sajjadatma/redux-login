91b8899efbe95553998d20c6c655e845db35abd9
sajjadatmae <sajjadatma2@gmail.comm>
2020-04-26 19:29:52 +0430

    Updates

---
+++ precache-manifest.c01bd3d3c9c4a8096b137d0b1ddb443e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08f267c397a4206567308641bacc012",
+    "url": "/redux-login/index.html"
+  },
+  {
+    "revision": "12877ee12e490c2751d9",
+    "url": "/redux-login/static/css/main.2b1a45a3.chunk.css"
+  },
+  {
+    "revision": "14c979c7a1ee8d85d432",
+    "url": "/redux-login/static/js/2.2d5b7910.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redux-login/static/js/2.2d5b7910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12877ee12e490c2751d9",
+    "url": "/redux-login/static/js/main.27d3df26.chunk.js"
+  },
+  {
+    "revision": "03094ea71228f6bef8b1",
+    "url": "/redux-login/static/js/runtime-main.70d85fb7.js"
+  },
+  {
+    "revision": "03db6b69ab0521eb4690ce3d08ae604e",
+    "url": "/redux-login/static/media/Login_card_image.03db6b69.jpg"
+  }
+]);
